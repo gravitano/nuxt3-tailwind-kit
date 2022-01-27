e6ec8b7cd67691a677db6e6d4ae99917bc47d732
Warsono <warsono16694@gmail.com>
2022-01-27 10:44:43 +0700

    feat: responsive

--- layouts/default.vue
+++ layouts/default.vue
@@ -40,23 +40,84 @@ const menus = ref([
     ],
   },
 ]);
+
+const mainMenu = ref(false);
+const sideMenu = ref(false);
 </script>
 
 <template>
   <div class="py-4 bg-white border-b dark:bg-gray-900">
-    <div class="container mx-auto flex justify-between gap-4">
+    <div class="container mx-auto flex justify-between gap-4 px-4 sm:px-0">
       <nuxt-link to="/" class="font-semibold text-primary-500 mr-5">
         Nuxt Tailwind Kit
       </nuxt-link>
-      <div class="space-x-3">
-        <nuxt-link class="hover:text-primary-500" to="/">Home</nuxt-link>
-        <nuxt-link class="hover:text-primary-500" to="/components">Components</nuxt-link>
+      <button class="appearance-none" @click="mainMenu = !mainMenu">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+        <span class="sr-only">Menu</span>
+      </button>
+      <div
+        class="sm:space-x-3 space-y-2"
+        :class="
+          mainMenu
+            ? 'block w-full fixed inset-x-0 px-4 shadow py-4 top-14 bg-white z-5'
+            : 'hidden sm:block'
+        "
+      >
+        <nuxt-link
+          class="hover:text-primary-500 block sm:inline"
+          @click="mainMenu = false"
+          to="/"
+        >
+          Home
+        </nuxt-link>
+        <nuxt-link
+          class="hover:text-primary-500 block sm:inline"
+          @click="mainMenu = false"
+          to="/components"
+        >
+          Components
+        </nuxt-link>
       </div>
     </div>
   </div>
-  <div class="container mx-auto xl:p-4 py-4 flex gap-4">
+  <div class="container mx-auto xl:p-4 py-4 flex flex-col sm:flex-row gap-4 px-4 sm:px-0">
     <div class="w-full sm:w-2/12">
-      <div v-for="(menu, i) in menus" :key="i" class="mb-5">
+      <button class="appearance-none sm:hidden" @click="sideMenu = !sideMenu">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h7"
+          />
+        </svg>
+        <span class="sr-only">Menu</span>
+      </button>
+      <div
+        v-for="(menu, i) in menus"
+        :key="i"
+        class="mb-5"
+        :class="sideMenu ? 'block' : 'hidden sm:block'"
+      >
         <h3 class="text-xs text-gray-500 uppercase mb-2">{{ menu.text }}</h3>
         <nuxt-link
           v-for="(child, j) in menu.children"
