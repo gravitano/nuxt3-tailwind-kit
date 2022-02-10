d9642f2438d3c320585f629b75e26f02d43efae4
Warsono <warsono16694@gmail.com>
2022-02-10 19:50:34 +0700

    feat: setup `@nuxtjs/tailwindcss`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,6 @@
 import { defineNuxtConfig } from 'nuxt3';
 
 export default defineNuxtConfig({
-  css: ['~/assets/css/tailwind.css'],
-  build: {
-    postcss: {
-      // add Postcss options
-      postcssOptions: require('./postcss.config.js'),
-    },
-  },
-  buildModules: [
-    '@vueuse/nuxt'
-  ]
+  buildModules: ['@vueuse/nuxt'],
+  modules: ['@nuxtjs/tailwindcss'],
 });
