a4a8e789ad60f5c885ea6e3ef19147664f4567ec
Warsono <warsono16694@gmail.com>
2022-02-10 20:16:29 +0700

    feat: sfc `moon-icon` and `sun-icon`

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,6 +2,8 @@
 import { ref } from "vue";
 import { useDark, useToggle } from "@vueuse/core";
 import MenuIcon from "./icons/MenuIcon.vue";
+import MoonIcon from "./icons/MoonIcon.vue";
+import SunIcon from "./icons/SunIcon.vue";
 
 const mainMenu = ref(false);
 
@@ -55,39 +57,8 @@ const toggleDark = useToggle(isDark);
           color="primary"
           class="!m-0 !p-0"
         >
-          <!-- moon -->
-          <svg
-            v-if="isDark"
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-            />
-          </svg>
-
-          <!-- sun -->
-          <svg
-            v-else
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-            />
-          </svg>
+          <moon-icon v-if="isDark" />
+          <sun-icon v-else />
         </v-btn>
       </div>
     </div>
