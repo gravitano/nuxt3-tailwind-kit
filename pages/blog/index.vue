c40fd668671b82616809ef2b209e027925363e57
Warsono <warsono16694@gmail.com>
2022-02-16 23:45:29 +0700

    chore: extract post item into SFC

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -22,30 +22,7 @@ try {
     <h1 class="text-3xl mb-1 font-bold">Blog</h1>
     <h2 class="text-xl mb-5 font-normal dark:text-gray-400">Latest Posts</h2>
 
-    <nuxt-link
-      class="mb-2 -mx-2 dark:hover:bg-gray-800 hover:bg-gray-100 p-2 rounded flex flex-col sm:flex-row gap-2 w-full"
-      :to="`/blog/posts/${post.attributes.slug}`"
-      v-for="post in posts"
-      :key="post.id"
-    >
-      <div class="flex-grow order-2 sm:order-1">
-        <h3 class="dark:text-gray-200 font-semibold text-2xl text-gray-800 mb-2 sm:mb-1">
-          {{ post.attributes.title }}
-        </h3>
-        <div class="text-xs dark:text-gray-500 text-gray-500 mb-2">
-          <span class="font-semibold">Author</span>
-          &middot; {{ new Date(post.attributes.publishedAt).toDateString() }}
-        </div>
-        <div class="text-sm dark:text-gray-400 text-gray-600">
-          {{ post.attributes.excerpt || post.attributes.content.substr(0, 150) }}
-        </div>
-      </div>
-      <blog-post-image
-        :image="post.attributes.image"
-        :alt="post.attributes.title"
-        class="w-full sm:w-4/12 object-cover rounded order-1 sm:order-2 flex-shrink-0"
-      />
-    </nuxt-link>
+    <BlogPostItem v-for="post in posts" :key="post.id" :post="post" />
   </div>
 </template>
 
