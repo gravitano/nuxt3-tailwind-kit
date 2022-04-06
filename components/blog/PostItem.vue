920440ebcc9ecc2ce18a86616dabb3df037197d9
Warsono <warsono16694@gmail.com>
2022-04-07 05:51:43 +0700

    fix: handle error on first load

--- components/blog/PostItem.vue
+++ components/blog/PostItem.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Post } from "~/types";
+import { Post } from '~/types';
 
 const props = defineProps<{
   post: Post;
@@ -8,11 +8,14 @@ const props = defineProps<{
 
 <template>
   <nuxt-link
+    v-if="post.attributes"
     class="mb-2 -mx-2 dark:hover:bg-gray-800 hover:bg-gray-100 p-2 rounded flex flex-col sm:flex-row gap-2 w-full"
     :to="`/blog/posts/${post.attributes.slug}`"
   >
     <div class="flex-grow order-2 sm:order-1">
-      <h3 class="dark:text-gray-200 font-semibold text-2xl text-gray-800 mb-2 sm:mb-1">
+      <h3
+        class="dark:text-gray-200 font-semibold text-2xl text-gray-800 mb-2 sm:mb-1"
+      >
         {{ post.attributes.title }}
       </h3>
       <div class="text-xs dark:text-gray-500 text-gray-500 mb-2">
