317a506b17a6b2a86d1215fcb0dff98d2924302b
Warsono <warsono16694@gmail.com>
2022-04-22 21:22:36 +0700

    chore: update docs collapsible

--- pages/docs/components/collapsibles.vue
+++ pages/docs/components/collapsibles.vue
@@ -1,60 +1,50 @@
 <script setup lang="ts">
-const items = ref([
-  {
-    title: 'Item 1',
+const genItems = (max = 5) =>
+  Array.from({ length: max }, (v, k) => ({
+    title: `Item ${k + 1}`,
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione expedita
       et veniam odio non. Facilis fugit qui error, optio pariatur provident
       vitae sint, possimus illo consequatur dignissimos velit quaerat voluptas!`,
-  },
-  {
-    title: 'Item 2',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione expedita
-      et veniam odio non. Facilis fugit qui error, optio pariatur provident
-      vitae sint, possimus illo consequatur dignissimos velit quaerat voluptas!`,
-  },
-]);
-const items2 = ref([
-  {
-    title: 'Item 1',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione expedita
-      et veniam odio non. Facilis fugit qui error, optio pariatur provident
-      vitae sint, possimus illo consequatur dignissimos velit quaerat voluptas!`,
-  },
-  {
-    title: 'Item 2',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione expedita
-      et veniam odio non. Facilis fugit qui error, optio pariatur provident
-      vitae sint, possimus illo consequatur dignissimos velit quaerat voluptas!`,
-  },
-]);
+  }));
+const items = ref(genItems());
+const items2 = ref(genItems());
 </script>
 
 <template>
-  <div>
-    <Collapsible title="Collapse">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione expedita
-      et veniam odio non. Facilis fugit qui error, optio pariatur provident
-      vitae sint, possimus illo consequatur dignissimos velit quaerat voluptas!
-    </Collapsible>
-
-    <CollapsibleGroup :items="items" />
-
-    <CollapsibleGroup :items="items2" accordion />
-
-    <CollapsibleGroup>
-      <Collapsible title="Collapse 1">
+  <div class="space-y-5">
+    <Card title="Default">
+      <Collapsible title="Collapse">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
         expedita et veniam odio non. Facilis fugit qui error, optio pariatur
         provident vitae sint, possimus illo consequatur dignissimos velit
         quaerat voluptas!
       </Collapsible>
-      <Collapsible title="Collapse 2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-        expedita et veniam odio non. Facilis fugit qui error, optio pariatur
-        provident vitae sint, possimus illo consequatur dignissimos velit
-        quaerat voluptas!
-      </Collapsible>
-    </CollapsibleGroup>
+    </Card>
+
+    <Card title="Items Prop">
+      <CollapsibleGroup :items="items" />
+    </Card>
+
+    <Card title="Accordion">
+      <CollapsibleGroup :items="items2" accordion />
+    </Card>
+
+    <Card title="Collapsible Group">
+      <CollapsibleGroup>
+        <Collapsible title="Collapse 1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          expedita et veniam odio non. Facilis fugit qui error, optio pariatur
+          provident vitae sint, possimus illo consequatur dignissimos velit
+          quaerat voluptas!
+        </Collapsible>
+        <Collapsible title="Collapse 2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          expedita et veniam odio non. Facilis fugit qui error, optio pariatur
+          provident vitae sint, possimus illo consequatur dignissimos velit
+          quaerat voluptas!
+        </Collapsible>
+      </CollapsibleGroup>
+    </Card>
   </div>
 </template>
 
