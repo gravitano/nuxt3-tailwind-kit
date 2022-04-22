1a11a595cbe58ce5111d1ed3ba052b239747f00e
Warsono <warsono16694@gmail.com>
2022-04-22 22:22:24 +0700

    chore: update responsive auth layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -2,11 +2,11 @@
 
 <template>
   <div class="flex flex-col sm:flex-row sm:h-screen">
-    <div class="w-full sm:w-5/12 p-6 bg-white">
+    <div class="w-full sm:w-6/12 lg:5/12 p-6 bg-white">
       <NuxtPage />
     </div>
     <div
-      class="bg-gradient-to-tr from-indigo-500 to-indigo-400 flex items-center justify-center p-6 w-full sm:w-7/12"
+      class="w-full sm:w-6/12 lg:7/12 bg-gradient-to-tr from-indigo-500 to-indigo-400 flex items-center justify-center p-6"
     >
       <LoginIllustration />
     </div>
