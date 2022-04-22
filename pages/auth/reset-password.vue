82b87310c25009286d8b1938e41c8468804e99aa
Warsono <warsono16694@gmail.com>
2022-04-22 22:21:15 +0700

    feat: reset password page

---
+++ pages/auth/reset-password.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import { useForm } from 'vee-validate';
+import { object, string } from 'yup';
+
+useHead({
+  title: 'Forgot Password',
+});
+
+definePageMeta({
+  layout: 'auth',
+});
+
+const { handleSubmit } = useForm({
+  validationSchema: object({
+    email: string().required().email().label('Email'),
+    password: string().required().label('Password'),
+    passwordConfirmation: string().required().label('Password Confirmation'),
+  }),
+});
+
+const onSubmit = handleSubmit((values) => {
+  alert(JSON.stringify(values, null, 2));
+});
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-full">
+    <form class="rounded-lg p-8 w-full max-w-md mx-auto" @submit="onSubmit">
+      <div class="mb-4 space-y-1">
+        <h1 class="text-4xl font-bold">Forgot Password</h1>
+        <p class="text-gray-500">Please enter your email</p>
+      </div>
+
+      <FormInput name="email" label="Email" placeholder="Email" />
+      <FormInput name="password" label="Password" placeholder="Password" />
+      <FormInput
+        name="passwordConfirmation"
+        label="Password Confirmation"
+        placeholder="Password Confirmation"
+      />
+
+      <v-btn type="submit" color="primary" block size="lg" class="mb-2">
+        Submit
+      </v-btn>
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
