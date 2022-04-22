8b3032134d261205c495137ffa6042fd173564ca
Warsono <warsono16694@gmail.com>
2022-04-22 22:19:51 +0700

    feat: register

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -3,7 +3,7 @@ import { useForm } from 'vee-validate';
 import { object, string } from 'yup';
 
 useHead({
-  title: 'Login',
+  title: 'Register',
 });
 
 definePageMeta({
@@ -12,6 +12,7 @@ definePageMeta({
 
 const { handleSubmit } = useForm({
   validationSchema: object({
+    name: string().required().label('Name'),
     email: string().required().email().label('Email'),
     password: string().required().label('Password'),
   }),
@@ -26,10 +27,11 @@ const onSubmit = handleSubmit((values) => {
   <div class="flex items-center justify-center h-full">
     <form class="rounded-lg p-8 w-full max-w-md mx-auto" @submit="onSubmit">
       <div class="mb-4 space-y-1">
-        <h1 class="text-4xl font-bold">Login</h1>
+        <h1 class="text-4xl font-bold">Register</h1>
         <p class="text-gray-500">Please enter your credentials</p>
       </div>
 
+      <FormInput name="name" label="Name" placeholder="Name" />
       <FormInput name="email" label="Email" placeholder="Email" />
       <FormInput
         name="password"
@@ -39,38 +41,33 @@ const onSubmit = handleSubmit((values) => {
       />
 
       <div class="mb-4 flex gap-2 justify-between items-center">
-        <label class="flex gap-2 items-center text-sm">
+        <label class="flex flex-wrap gap-2 items-center text-sm">
           <input
             type="checkbox"
             class="w-4 h-4 rounded text-primary-500 focus:ring-primary-500 transition duration-300"
           />
-          Remember me
+
+          <span> I agree with </span>
+          <nuxt-link
+            to="/toc"
+            class="text-primary-500 hover:underline font-semibold text-sm inline"
+          >
+            Terms and Condition
+          </nuxt-link>
         </label>
-        <nuxt-link
-          to="/auth/forgot-password"
-          class="text-primary-500 hover:underline font-semibold text-sm"
-        >
-          Forgot Password?
-        </nuxt-link>
       </div>
 
       <v-btn type="submit" color="primary" block size="lg" class="mb-2">
         Login
       </v-btn>
 
-      <!-- <button
-        class="px-3 py-2.5 rounded-md mb-4 font-semibold inline-flex w-full items-center justify-center bg-primary-500 text-white border-primary-500 focus:ring focus:ring-primary-500 focus:ring-offset-1 transition duration-300 hover:bg-primary-600 hover:border-primary-600"
-      >
-        Login
-      </button> -->
-
       <div class="text-gray-600 text-sm">
-        Don't have account?
+        Already have an account?
         <nuxt-link
-          to="/auth/register"
+          to="/auth/login"
           class="text-primary-500 hover:underline font-semibold text-sm"
         >
-          Register
+          Login
         </nuxt-link>
       </div>
     </form>
