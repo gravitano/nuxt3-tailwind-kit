ef39be39613eec32a88e99f7d7a25e540807297f
Warsono <warsono16694@gmail.com>
2022-05-24 17:22:39 +0700

    feat(swiper): add custom button example

--- pages/swiper.vue
+++ pages/swiper.vue
@@ -24,11 +24,11 @@ const modules = [Navigation, Pagination, A11y];
   <div>
     <swiper
       :modules="modules"
-      navigation
       :slides-per-view="1"
       :space-between="50"
       @swiper="onSwiper"
       @slideChange="onSlideChange"
+      class="relative"
     >
       <swiper-slide v-for="i in 5" :key="i">
         <HeroSection
@@ -53,5 +53,39 @@ const modules = [Navigation, Pagination, A11y];
         <HomeQuote class="text-center" :title="`Quote ${i}`" description="Lorem ipsum" />
       </swiper-slide>
     </swiper>
+
+    <div class="custom-swiper relative mt-10 bg-blue-50">
+      <button
+        class="swiper-btn-prev btn btn-primary rounded-full z-10 absolute top-1/2 left-2 transform -translate-y-1/2"
+      >
+        Prev
+      </button>
+      <button
+        class="swiper-btn-next btn btn-primary rounded-full z-10 absolute top-1/2 right-2 transform -translate-y-1/2"
+      >
+        Next
+      </button>
+
+      <swiper
+        :modules="modules"
+        :navigation="{
+          nextEl: '.swiper-btn-next',
+          prevEl: '.swiper-btn-prev',
+        }"
+        :pagination="{ clickable: true }"
+        :slides-per-view="1"
+        :space-between="50"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+      >
+        <swiper-slide v-for="i in 5" :key="i">
+          <HomeQuote
+            class="text-center"
+            :title="`Quote ${i}`"
+            description="Lorem ipsum"
+          />
+        </swiper-slide>
+      </swiper>
+    </div>
   </div>
 </template>
