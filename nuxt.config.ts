837fcacbad2db13279965eebd3e6ca384d3731cf
Warsono <warsono16694@gmail.com>
2022-08-22 12:26:27 +0700

    chore: upgrade to nuxt RC 8

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,15 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     '@nuxtjs/strapi',
     ['unplugin-icons/nuxt', {}],
-    ['@pinia/nuxt', { disableVuex: true }],
     '@intlify/nuxt3',
   ],
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/content',
+    '@pinia/nuxt',
+  ],
   strapi: {
-    url: process.env.STRAPI_URL || 'https://strapi.warsono.id',
+    url: process.env.STRAPI_URL || 'https://bapi.warsono.id',
     prefix: '/api',
     version: 'v4',
     cookie: {},
