c86fa60ce1fab52a5edfd69bd29835e406381a36
Warsono <warsono16694@gmail.com>
2022-08-30 10:05:27 +0700

    feat: add GA

--- app.vue
+++ app.vue
@@ -7,6 +7,15 @@
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet"
       />
+      <!-- Google tag (gtag.js) -->
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-0XXRF7JV2H"></Script>
+      <Script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-0XXRF7JV2H');
+      </Script>
     </Head>
 
     <NuxtLayout>
