3025c4992b0b28ef23a4658942b10165ec9ba2e5
Warsono <warsono16694@gmail.com>
2023-02-04 15:00:17 +0700

    fix: hide mobile menu when page change

--- components/Home/MobileNav.vue
+++ components/Home/MobileNav.vue
@@ -29,12 +29,21 @@ const mobileMenu = useMobileMenu()
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
-              <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">
+              <NuxtLink
+                v-for="item in navigation" :key="item.name" :to="item.href" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                @click="mobileMenu = false"
+              >
                 {{ item.name }}
               </NuxtLink>
             </div>
             <div class="py-6">
-              <a href="#" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10">Log in</a>
+              <NuxtLink
+                to="/auth/login"
+                class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
+                @click="mobileMenu = false"
+              >
+                Log in
+              </NuxtLink>
             </div>
           </div>
         </div>
