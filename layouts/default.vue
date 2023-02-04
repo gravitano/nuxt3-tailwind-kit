a85fd2e259579ed681cf3330cbdedd0b81a21433
Warsono <warsono16694@gmail.com>
2023-02-04 14:41:46 +0700

    chore: fix z index

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <HomeHeader hide-hero />
-    <div class="flex-1 container mx-auto">
+    <div class="flex-1 container mx-auto z-10">
       <slot />
     </div>
     <AppFooter />
