f03111a27075aebc9dab25885a317ce5d3b3642b
Warsono <warsono16694@gmail.com>
2023-02-04 16:39:05 +0700

    feat: improve page header styles

--- components/PageHeader/PageHeader.vue
+++ components/PageHeader/PageHeader.vue
@@ -8,11 +8,11 @@ defineProps<Props>()
 </script>
 
 <template>
-  <div class="mb-5 space-y-1">
-    <h1 class="font-bold text-2xl text-gray-900 dark:text-gray-200">
+  <div class="mb-5 space-y-1 py-10">
+    <h1 class="font-bold text-2xl text-gray-800 dark:text-gray-200">
       {{ title }}
     </h1>
-    <p v-if="subtitle" class="text-gray-700 text-base dark:text-gray-400">
+    <p v-if="subtitle" class="text-gray-600 text-lg dark:text-gray-400">
       {{ subtitle }}
     </p>
   </div>
