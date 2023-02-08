ddaf419aa7a6b64b52386d9df0e2b7695492c847
Warsono <warsono16694@gmail.com>
2023-02-08 23:52:18 +0700

    feat(Select): add multiple select support

--- components/global/Select/Select.vue
+++ components/global/Select/Select.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, toRefs, watch } from 'vue'
+import { PropType, ref, toRefs, watch } from 'vue'
 import {
   Listbox,
   ListboxButton,
@@ -8,45 +8,66 @@ import {
 } from '@headlessui/vue'
 import type { SelectItem } from './types'
 
-interface Props {
-  modelValue: SelectItem
-  items: SelectItem[]
-  placeholder?: string
-  hideCheckIcon?: boolean
-  outlined?: boolean
-  large?: boolean
-  small?: boolean
-}
+type ModelValue = SelectItem | SelectItem[] | null | undefined
 
-const props = withDefaults(defineProps<Props>(), {
-  items: () => [],
-  placeholder: 'Choose',
-  hideCheckIcon: false,
-  outlined: false,
-  large: false,
-  small: false,
+const props = defineProps({
+  modelValue: {
+    type: Object as PropType<ModelValue>,
+  },
+  items: {
+    type: Array as PropType<SelectItem[]>,
+    default: () => [] as SelectItem[],
+  },
+  placeholder: {
+    type: String,
+    default: 'Choose',
+  },
+  hideCheckIcon: {
+    type: Boolean,
+    default: false,
+  },
+  outlined: {
+    type: Boolean,
+    default: false,
+  },
+  large: {
+    type: Boolean,
+    default: false,
+  },
+  small: {
+    type: Boolean,
+    default: false,
+  },
+  multiple: {
+    type: Boolean,
+    default: false,
+  },
 })
 
-const emit = defineEmits(['update:modelValue'])
+const emit = defineEmits(['update:modelValue', 'clear'])
 
 const { modelValue } = toRefs(props)
 
-const selected = ref<SelectItem | undefined>(modelValue.value)
+const selected = ref<ModelValue>(modelValue!.value)
 
-watch(modelValue, (val) => {
+watch(modelValue!, (val) => {
   selected.value = val
 })
 
 watch(selected, (val) => {
   emit('update:modelValue', val)
 })
+
+function clear() {
+  selected.value = undefined
+  emit('clear')
+}
 </script>
 
 <template>
-  <Listbox v-model="selected" class="w-full">
+  <Listbox v-model="selected" class="w-full" :multiple="multiple">
     <div class="relative mt-1">
-      <ListboxButton
-        class="
+      <ListboxButton class="
           relative
           w-full
           py-2
@@ -64,8 +85,7 @@ watch(selected, (val) => {
           focus-visible:border-primary-500
           transition
           duration-300
-        "
-        :class="[
+        " :class="[
           outlined
             ? 'bg-transparent border-icon-inverse text-inverse-subdued'
             : 'bg-white',
@@ -74,35 +94,31 @@ watch(selected, (val) => {
             'text-sm': !small && !large,
             'text-lg': large,
           }
-        ]"
-      >
-        <span class="block truncate">{{ selected?.text || placeholder }}</span>
-        <span
-          class="
-            absolute
-            inset-y-0
-            right-0
+        ]">
+        <span class="block truncate">
+          {{ multiple && selected ? `${(selected as SelectItem[])?.length} selected` : (selected as SelectItem)?.text ||
+  placeholder
+          }}
+        </span>
+
+        <div class="absolute inset-y-0 right-3 flex gap-2 items-center">
+          <button v-if="multiple ? (selected as SelectItem[])?.length > 0 : selected" type="button" aria-label="Clear"
+            @click="clear" class="text-gray-500 hover:text-gray-700">
+            <Icon name="heroicons:x-mark-20-solid" class="h-5 w-5" aria-hidden="true" />
+          </button>
+          <span class="
             flex
             items-center
-            pr-2
             pointer-events-none
-          "
-        >
-          <Icon
-            name="heroicons:chevron-up-down"
-            class="w-5 h-5"
-            aria-hidden="true"
-          />
-        </span>
+          ">
+            <Icon name="heroicons:chevron-up-down" class="w-5 h-5" aria-hidden="true" />
+          </span>
+        </div>
       </ListboxButton>
 
-      <transition
-        leave-active-class="transition duration-100 ease-in"
-        leave-from-class="opacity-100"
-        leave-to-class="opacity-0"
-      >
-        <ListboxOptions
-          class="
+      <transition leave-active-class="transition duration-100 ease-in" leave-from-class="opacity-100"
+        leave-to-class="opacity-0">
+        <ListboxOptions class="
             absolute
             z-10
             w-full
@@ -117,34 +133,21 @@ watch(selected, (val) => {
             ring-1 ring-black ring-opacity-5
             focus:outline-none
             sm:text-sm
-          "
-        >
-          <ListboxOption
-            v-for="item in items"
-            v-slot="{ active, selected: selectedOption }"
-            :key="item.text"
-            :value="item"
-            as="template"
-          >
+          ">
+          <ListboxOption v-for="item in items" v-slot="{ active, selected: selectedOption }" :key="item.text"
+            :value="item" as="template">
             <div class="px-1">
               <li v-if="item.divider" class="border-b -mx-1 my-1" />
-              <li
-                v-else
-                class="cursor-default select-none relative py-2 pr-4 rounded" :class="[
-                  active ? 'bg-gray-100' : 'text-gray-900',
-                  !hideCheckIcon ? 'pl-10' : 'pl-4',
-                ]"
-              >
-                <span
-                  class="block truncate" :class="[
-                    selectedOption ? 'font-medium text-primary-500' : 'font-normal',
-                  ]"
-                >
+              <li v-else class="cursor-default select-none relative py-2 pr-4 rounded" :class="[
+                active ? 'bg-gray-100' : 'text-gray-900',
+                !hideCheckIcon ? 'pl-10' : 'pl-4',
+              ]">
+                <span class="block truncate" :class="[
+                  selectedOption ? 'font-medium text-primary-500' : 'font-normal',
+                ]">
                   {{ item.text }}
                 </span>
-                <span
-                  v-if="selectedOption && !hideCheckIcon"
-                  class="
+                <span v-if="selectedOption && !hideCheckIcon" class="
                     absolute
                     inset-y-0
                     left-0
@@ -152,13 +155,8 @@ watch(selected, (val) => {
                     items-center
                     pl-3
                     text-primary-500
-                  "
-                >
-                  <Icon
-                    name="heroicons:check"
-                    class="w-5 h-5"
-                    aria-hidden="true"
-                  />
+                  ">
+                  <Icon name="heroicons:check" class="w-5 h-5" aria-hidden="true" />
                 </span>
               </li>
             </div>
--- components/global/Select/types.d.ts
+++ components/global/Select/types.d.ts
@@ -2,4 +2,4 @@ export type SelectItem = {
   text: string;
   value: string | number;
   divider?: boolean;
-};
+}
