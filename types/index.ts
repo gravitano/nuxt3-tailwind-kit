b863f8a4c35cbad4c2aacb11fb0a3be7c24ca18a
Warsono <warsono16694@gmail.com>
2023-03-04 13:25:26 +0700

    chore: clean up some files

--- types/index.ts
+++
@@ -1,7 +0,0 @@
-export interface Post extends Record<string, any> {}
-
-export interface NavigationItem extends Record<string, any> {
-  title: string
-  to: string
-  children?: NavigationItem[]
-}
