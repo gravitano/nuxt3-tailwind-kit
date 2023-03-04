7fea23744326e7d8b227aef0a9dd0f70b0f0c3c4
Warsono <warsono16694@gmail.com>
2023-03-04 14:19:12 +0700

    fix: update swiper example

--- apps/docs/pages/docs/swiper.vue
+++ apps/docs/pages/docs/swiper.vue
@@ -33,11 +33,10 @@ const modules = [Navigation, Pagination, A11y]
       @slideChange="onSlideChange"
     >
       <SwiperSlide v-for="i in 5" :key="i">
-        <HeroSection
-          :title="`Slide ${i}`"
+        <LandingQuote
+          class="text-center"
+          :title="`Quote ${i}`"
           description="Lorem ipsum"
-          :image="`https://picsum.photos/1920/1080?random=${i}`"
-          :button="{ text: 'Get Started', action: '#' }"
         />
       </SwiperSlide>
     </Swiper>
@@ -52,21 +51,25 @@ const modules = [Navigation, Pagination, A11y]
       @slideChange="onSlideChange"
     >
       <SwiperSlide v-for="i in 5" :key="i">
-        <HomeQuote class="text-center" :title="`Quote ${i}`" description="Lorem ipsum" />
+        <LandingQuote class="text-center" :title="`Quote ${i}`" description="Lorem ipsum" />
       </SwiperSlide>
     </Swiper>
 
     <div class="custom-swiper relative mt-10 bg-blue-50">
-      <button
-        class="swiper-btn-prev btn btn-icon shadow-md btn-primary rounded-full z-10 absolute top-1/2 -left-5 transform -translate-y-1/2"
+      <VButton
+        size="lg"
+        shadow
+        class="w-10 !h-10 px-0 py-0 rounded-full swiper-btn-prev z-10 absolute top-1/2 -left-5 transform -translate-y-1/2"
       >
         <Icon class="w-5 h-5" name="heroicons:chevron-left-20-solid" />
-      </button>
-      <button
-        class="swiper-btn-next btn btn-icon shadow-md btn-primary rounded-full z-10 absolute top-1/2 -right-5 transform -translate-y-1/2"
+      </VButton>
+      <VButton
+        size="lg"
+        shadow
+        class="w-10 !h-10 px-0 py-0 rounded-full swiper-btn-next z-10 absolute top-1/2 -right-5 transform -translate-y-1/2"
       >
         <Icon class="w-5 h-5" name="heroicons:chevron-right-20-solid" />
-      </button>
+      </VButton>
 
       <Swiper
         :modules="modules"
@@ -81,7 +84,7 @@ const modules = [Navigation, Pagination, A11y]
         @slideChange="onSlideChange"
       >
         <SwiperSlide v-for="i in 5" :key="i">
-          <HomeQuote
+          <LandingQuote
             class="text-center"
             :title="`Quote ${i}`"
             description="Lorem ipsum"
