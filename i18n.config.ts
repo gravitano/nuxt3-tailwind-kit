047179ba8e10afc86e71d1f987c6a352e7fdbadb
Warsono <warsono16694@gmail.com>
2023-04-16 17:34:40 +0700

    chore: revert nuxt i18n to 8.0.0-beta.10 for now

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,24 +1,24 @@
-export default defineI18nConfig(() => ({
-  legacy: false,
-  locale: 'en',
-  messages: {
-    en: {
-      app_name: 'Nuxt Tailwind Kit',
-      app_description:
-        'Quick Boilerplate built on top of Nuxt 3 and Tailwind CSS',
-      menu_home: 'Home',
-      menu_store: 'Store',
-      menu_blog: 'Blog',
-      menu_dashboard: 'Dashboard',
-    },
-    id: {
-      app_name: 'Nuxt Tailwind Kit',
-      app_description:
-        'Boilerplate cepat yang dibangun dari Nuxt 3 and Tailwind CSS',
-      menu_home: 'Beranda',
-      menu_store: 'Toko',
-      menu_blog: 'Blog',
-      menu_dashboard: 'Dasbor',
-    },
-  },
-}))
+// export default defineI18nConfig(() => ({
+//   legacy: false,
+//   locale: 'en',
+//   messages: {
+//     en: {
+//       app_name: 'Nuxt Tailwind Kit',
+//       app_description:
+//         'Quick Boilerplate built on top of Nuxt 3 and Tailwind CSS',
+//       menu_home: 'Home',
+//       menu_store: 'Store',
+//       menu_blog: 'Blog',
+//       menu_dashboard: 'Dashboard',
+//     },
+//     id: {
+//       app_name: 'Nuxt Tailwind Kit',
+//       app_description:
+//         'Boilerplate cepat yang dibangun dari Nuxt 3 and Tailwind CSS',
+//       menu_home: 'Beranda',
+//       menu_store: 'Toko',
+//       menu_blog: 'Blog',
+//       menu_dashboard: 'Dasbor',
+//     },
+//   },
+// }))
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,31 @@ export default defineNuxtConfig({
   i18n: {
     locales: ['en', 'id'],
     defaultLocale: 'en',
-    vueI18n: './i18n.config.ts', // if you are using custom path, default
+    vueI18n: {
+      legacy: false,
+      locale: 'en',
+      messages: {
+        en: {
+          app_name: 'Nuxt Tailwind Kit',
+          app_description:
+        'Quick Boilerplate built on top of Nuxt 3 and Tailwind CSS',
+          menu_home: 'Home',
+          menu_store: 'Store',
+          menu_blog: 'Blog',
+          menu_dashboard: 'Dashboard',
+        },
+        id: {
+          app_name: 'Nuxt Tailwind Kit',
+          app_description:
+        'Boilerplate cepat yang dibangun dari Nuxt 3 and Tailwind CSS',
+          menu_home: 'Beranda',
+          menu_store: 'Toko',
+          menu_blog: 'Blog',
+          menu_dashboard: 'Dasbor',
+        },
+      },
+    },
+    // vueI18n: './i18n.config.ts', // if you are using custom path, default
   },
   runtimeConfig: {
     public: {
