c20ffb7202527c649eb67990193828f67c592e91
Kamil Kras <k.kras@digitalcolliers.com>
2023-04-16 15:26:07 +0200

    revert changes in admin.vue

--- apps/admin/layouts/admin.vue
+++ apps/admin/layouts/admin.vue
@@ -8,28 +8,6 @@ useHead({
 })
 
 const sidebar = useAdminSidebar()
-T
-const fun = (x: number) => {
-  if (x === 1)
-    return 'a'
-  if (x === 2)
-    return 'b'
-  if (x === 3)
-    return 'c'
-  if (x === 4)
-    return 'd'
-  if (x === 5)
-    return 'e'
-  if (x === 6)
-    return 'f'
-  if (x === 7)
-    return 'g'
-  if (x === 8)
-    return 'h'
-  if (x === 9)
-    return 'i'
-  return 'z'
-}
 </script>
 
 <template>
