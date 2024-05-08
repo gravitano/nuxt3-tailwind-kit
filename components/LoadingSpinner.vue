84e4cb02cb311d5fa29b2420c9b5703b5663f92c
kazGuido <kazihise.guy@gmail.com>
2024-05-08 11:03:35 +0800

    starting

---
+++ components/LoadingSpinner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="spinner" v-if="loading">
+    <!-- You can use any CSS framework spinner or a custom SVG -->
+    <img src="/spinner.gif" alt="Loading...">
+  </div>
+</template>
+
+<script setup>
+// You can use a reactive state to control visibility
+const props = defineProps({
+  loading: Boolean
+})
+</script>
+
+<style scoped>
+.spinner {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
